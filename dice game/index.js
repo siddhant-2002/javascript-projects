edbf1ea680b76f5870e5b3cf48183d4863d6051b
siddhant-2002 <siddeshmukh711@gmail.com>
2024-04-24 13:30:56 +0530

    first commit

---
+++ dice game/index.js
@@ -0,0 +1,27 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+
+var imageName = "dice" + randomNumber1 + ".png";
+
+var imageSource = "images/" + imageName;
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", imageSource);
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var imageName2 = "dice" + randomNumber2 + ".png";
+
+var randomImageSource2 = "images/" + imageName2;
+
+var image2 = document.querySelectorAll("img")[1];
+
+image2.setAttribute("src", randomImageSource2);
+
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "player 1 wins...";
+} else if (randomNumber2 > randomNumber1) {
+  document.querySelector("h1").innerHTML = "player 2 wins...";
+} else {
+  document.querySelector("h1").innerHTML = "Draw Match...";
+}
